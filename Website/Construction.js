d1872afc395fc9f6fe842fcf4e8b2b5175d47a30
TheThunderGuyS <saihaj.sran@gmail.com>
2019-09-14 19:29:58 -0400

    Webpage Adjustments

---
+++ Website/Construction.js
@@ -0,0 +1,3 @@
+// Why are you even here?
+console.log("This homepage is under heavy construction and there is no reason you should be here.");
+console.log("View the project on GitHub: https://github.com/TheThunderGuyS/OldMinecraftSounds/");
