0ac16a54b4e108695988f1a23bf862d99614a5c8
TheThunderGuyS <saihaj.sran@gmail.com>
2019-09-19 20:20:24 -0400

    stuff

--- Website/Construction.js
+++
@@ -1,3 +0,0 @@
-// TheThunderGuy_S was here. Why are *you* here? Go away. Stop looking at my hideous "code"!
-console.info("This website is highly incomplete and there isn't much to do here.");
-console.log("View this project on GitHub: https://github.com/TheThunderGuyS/OldMinecraftSounds/");
