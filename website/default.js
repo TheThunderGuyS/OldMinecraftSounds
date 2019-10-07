6c89334bf5f2c552149d176bc46b7e023bc9fc93
TheThunderGuyS <saihaj.sran@gmail.com>
2019-10-07 17:41:03 -0400

    html > sounds.json

---
+++ website/default.js
@@ -0,0 +1,2 @@
+console.log("*This website is highly incomplete* \nView this project on GitHub: \nhttps://github.com/TheThunderGuyS/OldMinecraftSounds/");
+var clickSound=new Audio("website/click.mp3");
